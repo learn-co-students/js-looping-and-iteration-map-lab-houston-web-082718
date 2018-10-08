9934306a60a90f8e990f7aafb1520e02f769a1bc
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-08 11:31:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+let drivers = []
+function lowerCaseDrivers(drivers) {
+  return drivers.map( function (name) { 
+    return name.toLowerCase();
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (name) {
+    const nameArray = name.split(' ');
+
+    return { firstName: nameArray[0], lastName: nameArray[1]}
+    });
+  }
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  });
+  }
