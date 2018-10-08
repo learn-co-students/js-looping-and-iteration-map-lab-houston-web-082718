2c699af7b51e7232fb09193e6abbdd48fc9e7983
Flatiron School <andrewsprague@outlook.com>
2018-10-08 10:46:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+function lowerCaseDrivers (drivers) {
+    return drivers.map(driver => {
+        return driver.toLowerCase();
+    });
+};
+
+function nameToAttributes(drivers) {
+    return drivers.map(driver => {
+        let driverName = {};
+        let names = driver.split(' ');
+        driverName.firstName = names[0];
+        driverName.lastName = names[1];
+        return driverName;
+    });
+};
+
+function attributesToPhrase(drivers) {
+    return drivers.map(driver => {
+        return `${driver.name} is from ${driver.hometown}`;
+    });
+};
